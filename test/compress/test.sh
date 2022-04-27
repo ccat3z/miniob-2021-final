c352e0906b5f33bac353f28975dc423c71eb6473
ccat3z <fzhang.chn@foxmail.com>
2022-04-27 07:03:33 +0000

    compress test: support custom data

--- test/compress/test.sh
+++ test/compress/test.sh
@@ -7,6 +7,11 @@ REPO_DIR="$TEST_DIR/../.."
 BUILD_DIR="$REPO_DIR/build"
 OBSERVER="$BUILD_DIR/bin/observer"
 OBCLIENT="$BUILD_DIR/bin/obclient"
+TABLE_DATA="$1"
+
+if [ -z "$TABLE_DATA" ]; then
+    TABLE_DATA="miniob-sample.table"
+fi
 
 info() {
     echo -e "\e[1m> $@\e[0m"
@@ -43,6 +48,6 @@ fi
 
 # paylaod
 sql "create table t1 (c1 int, c2 int, c3 int, v1 char, v2 char, v3 char, v4 char, v5 char, v6 char, v7 char, v8 char, v9 char);"
-sql "load data infile '$TEST_DIR/miniob-sample.table' into table t1;"
+sql "load data infile '$TEST_DIR/$TABLE_DATA' into table t1;"
 sql "create index i1 on t1(c2);"
 sql "select * from t1 where c1 = 1;"
